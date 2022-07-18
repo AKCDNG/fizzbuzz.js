82a8fe459bc7eb20743d917d6dfd7a8626864669
Anish <anishkakaiya@outlook.com>
2022-07-18 22:40:27 +0100

    change test from input of 5 to input of 4

--- fizzbuzz.test.js
+++ fizzbuzz.test.js
@@ -5,7 +5,7 @@ describe('FizzBuzz Testing', () => {
     expect(fizzbuzzCalculator(3)).toEqual([1, 2, "Fizz"])
   })
 
-  it('Returns "Fizz" at 3rd index and also returns up to 5 in array when input is 5', () => {
-    expect(fizzbuzzCalculator(5)).toEqual([1, 2, "Fizz", 4, 5])
+  it('Returns "Fizz" at 3rd index and also returns up to 4 in array when input is 4', () => {
+    expect(fizzbuzzCalculator(5)).toEqual([1, 2, "Fizz", 4])
   })
 })
