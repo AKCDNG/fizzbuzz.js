2d237f0c2323f88fed919cfcd59ce75ccb4ca5f9
Anish <anishkakaiya@outlook.com>
2022-07-18 22:39:31 +0100

    add second test, break assumption of length of array

--- fizzbuzz.test.js
+++ fizzbuzz.test.js
@@ -1,5 +1,11 @@
+require('./fizzbuzz')
+
 describe('FizzBuzz Testing', () => {
   it('Returns "Fizz" at 3rd index of array with input of 3', () => {
-    expect(fizzbuzz_calculator(3))
+    expect(fizzbuzzCalculator(3)).toEqual([1, 2, "Fizz"])
+  })
+
+  it('Returns "Fizz" at 3rd index and also returns up to 5 in array when input is 5', () => {
+    expect(fizzbuzzCalculator(5)).toEqual([1, 2, "Fizz", 4, 5])
   })
 })
